b8f065e3b934c5f000761bf8654dd86024fe52ab
Lepsi <vdomoevskiy@phoenixit.ru>
2024-03-24 02:28:55 +0300

    Че то поменял, вроде подключение к бд и название таблицы

--- conf/config.py
+++ conf/config.py
@@ -3,7 +3,12 @@ from decouple import config
 
 class ConfigDatabase(object):
     DB_HOST = config('DB_HOST', default='localhost')
-    DB_PORT = config('')
+    DB_PORT = config('DB_PORT', default='5555')
     DB_NAME = config('DB_NAME', default='ratetracker')
     DB_USER = config('DB_USER', default='postgres')
-    DB_PASSWORD = config('DB_PASSWORD', default='')
+    DB_PASSWORD = config('DB_PASSWORD', default='8521946733')
+    CONNECT_INFO_FIELD = config('CONNECT_INFO_FIELD', default='trade_info')
+
+
+class Broker(object):
+    BROKER_URL = config('BROKER_URL', default='amqp://guest:guest@localhost//')
