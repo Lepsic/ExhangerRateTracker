9b1cc56b548b0b043cd11031eb73e8ff6c49e1af
Lepsi <vdomoevskiy@phoenixit.ru>
2024-03-24 16:49:13 +0300

    Обновил wrapper для(Понял что можно обойтись одним)

--- task/wrappers/wrapper.py
+++ task/wrappers/wrapper.py
@@ -7,30 +7,42 @@ from db.connect_info.get.get import get_connection_info
 from db.connect_info.schema.get.connection_info import ConnectionInfo
 from task.base.base import BaseTask
 from importlib import import_module
-from task.ws_task.base import WebSocketTaskBase
+from rabbit.queue.get import get_queue_name
+from rabbit.queue.create import CreateQueue
+import aio_pika
+import json
+
+
+
 
 class TaskWrapper(BaseWrapper):
 
     def __init__(self, *args, **kwargs):
         super(TaskWrapper, self).__init__()
+        self.creator_queue = CreateQueue()
 
-    async def task(self, instance: BaseTask, ws: WebSocketClientProtocol, channel: str, routing_key: str):
+    async def task(self, instance: BaseTask, ws: WebSocketClientProtocol, channel: str):
         """Queue - очередь на брокере"""
         reconnect_count = 0
         try:
             while True:
-                price = await instance.execute(ws=ws)
-                price = str(price)
+                data = await instance.execute(ws=ws)
+                logger.debug(data)
                 # await channel.default_exchange.publish(
                 #     aio_pika.Message(body=price),
                 #     routing_key='my_queue'
                 # )
-                await asyncio.sleep(3)
+                for key, value in data["data"].items():
+                    queue = self.creator_queue.queue_dict.get(key)
+                    if queue is not None:
+                        message = aio_pika.Message(body=json.dumps(value).encode())
+                        await queue.channel.default_exchange.publish(message=message, routing_key=queue.name)
+                await asyncio.sleep(5)
         except websockets.exceptions.WebSocketException as error:
             reconnect_count += 1
             logger.warning(f"Reconnecting \n try {reconnect_count}")
             ws = await instance.connection()
-            await self.task(instance=instance, ws=ws, channel=channel, routing_key=routing_key)
+            await self.task(instance=instance, ws=ws, channel=channel)
             logger.warning(f"Reconnecting try {reconnect_count} failed")
             if reconnect_count == 10:
                 logger.critical(f"Reconnecting failed raise Exception {error}")
@@ -39,12 +51,13 @@ class TaskWrapper(BaseWrapper):
             logger.error(f"Unknown error {error}")
 
     async def create_task_classes(self) -> list[BaseTask]:
+
         instances = await self.preproccess()
         tasks = []
         for instance in instances:
             task = self.get_class_instance(instance.task_type)
-            task_instance = task(url=instance.url, currency_pair=instance.currency_pair, channel="test",
-                                 routing_key="test")
+            task_instance = task(url=instance.url, currency_pair=instance.currency_pair,
+                                 channel=await self.creator_queue.create(instance))
             tasks.append(task_instance)
         return tasks
 
@@ -53,9 +66,9 @@ class TaskWrapper(BaseWrapper):
     async def task_creation(self, *args, **kwargs) -> list:
         tasks = []
         for task in await self.create_task_classes():
-            if isinstance(task, WebSocketTaskBase):
-                ws = await task.connection()
-                tasks.append(self.task(instance=task, ws=ws, channel="test", routing_key="test"))
+            ws = await task.connection()
+            tasks.append(self.task(instance=task, ws=ws, channel="test"))
+
         return tasks
 
     async def task_startup(self):
@@ -71,3 +84,4 @@ class TaskWrapper(BaseWrapper):
         module_name, class_name = path.rsplit(":", 1)
         module = import_module(module_name)
         return getattr(module, class_name)
+
