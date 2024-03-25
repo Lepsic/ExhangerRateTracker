9e4bbd71bc0c31d161e516fe71001266d7888e86
Lepsi <vdomoevskiy@phoenixit.ru>
2024-03-25 06:05:28 +0300

    основное - адаптация под докер ну и по мелочи там сям

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from aio_pika import Message
 from fastapi import FastAPI
 import uvicorn
-import websockets
-from task.wrappers.wrapper import TaskWrapper
-import asyncio
-from db.db_startup import startup_database
 import aio_pika
-from loguru import logger
-from  rabbit.consumer.consumer import Consumer
+from rabbit.consumer.consumer import Consumer
+from api.v1.enpoints.get import router as router_v1
+from rabbit.queue.create import CreateQueue
+from db.connect_info.get.get import get_connection_info
+from db.db_startup import startup_database
+from parser_service.wrappers.wrapper import TaskWrapper
+import asyncio
 
 app = FastAPI()
 
 
 # @app.get("/")
 # async def connect_to_binance_ws():
-#     async with websockets.connect('wss://stream.binance.com:9443/ws/btcusdt@trade') as websocket:
+#     async with websockets.connection('wss://stream.binance.com:9443/ws/btcusdt@trade') as websocket:
 #         while True:
 #             data = await websocket.recv()
 #             print(data)
@@ -23,6 +23,7 @@ app = FastAPI()
 async def index():
     return {"message": "hello"}
 
+app.include_router(router_v1, prefix="/api")
 
 async def consume(message: aio_pika.Message):
     async with message.process():
@@ -32,14 +33,13 @@ async def consume(message: aio_pika.Message):
 
 @app.on_event("startup")
 async def startup():
+    await startup_database()
+    creator = CreateQueue()
+    for info in await get_connection_info():
+        await creator.create(info)
     consumer = Consumer()
     await consumer.start()
-    # connection = await aio_pika.connect_robust("amqp://guest:guest@localhost/")
-    # channel = await connection.channel()
-    # queue = await channel.get_queue("bitcoin/rub")
-    # queue1 = await channel.get_queue("bitcoin/usd")
-    # await queue.consume(callback=consume)
-    # await queue1.consume(callback=consume)
+
 
 
 
